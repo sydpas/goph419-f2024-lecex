62e2d7e9fedfa29b057aa16ff34a640d66b0dcaf
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-10-28 17:24:06 -0600

    modified array to include forward subst and GE

--- src/topic2/array.py
+++ src/topic2/array.py
@@ -1,20 +1,56 @@
 import numpy as np
 
-from src.topic2.linalg import forward_subst
+from src.topic2.linalg import (
+    forward_subst,
+    backward_subst,
+    gauss_solve
+)
 
 def main():
-    A = np.array([[8e5, 0, 0, 0],
-                  [-8e5, 2e6, 0, 0],
-                  [0, -2e6, 8e6, 0],
-                  [0, 0, -8e6, 2e7]]
-                 )
+    # testing lower tri
+    A = np.array(
+        [[8e5, 0, 0, 0], [-8e5, 2e6, 0, 0], [0, -2e6, 8e6, 0], [0, 0, -8e6, 2e7]]
+    )
     b = np.array([1.0, 2.0, 3.0, 4.0])
-    print("Solving Ax = b...")
+    print("Solving lower triangular Ax = b...")
     print(f"A: {A}")
     print(f"b: {b}")
 
     x_np = np.linalg.solve(A, b)
     print(f"The solution is: {x_np}")
+
+    for_sub = forward_subst(A, b)
+    print(f"The forward_subst algorithm gives: {for_sub}")
+
+    # testing upper tri
+    A = np.array(
+        [[4, -1, -1, -1], [0, 5, -1, -2], [0, 0, 2, -1], [0, 0, 0, 8]]
+    )
+    b = np.array([[1.0, 2.0, 3.0, 4.0], [2.0, 4.0, 6.0, 8.0]]).T  # T is transpose matrix
+    print("Solving upper triangular Ax = b...")
+    print(f"A: {A}")
+    print(f"b: {b}")
+
+    x_np = np.linalg.solve(A, b)
+    print(f"The solution is: {x_np}")
+
+    back_sub = backward_subst(A,b)
+    print(f"The backward_subst algorithm gives: {back_sub}")
+
+    # full rank non tri system
+    A = np.array(
+        [[2.0, 1.0, 0.0, 0.0], [-1.0, 2.0, -1.0, 0.0], [0.0, -2.0, 4.0, -1.0], [0.0, 0.0, -1.0, 1.0]]
+    )
+    b = np.array([[1.0, 2.0, 3.0, 4.0], [2.0, 4.0, 6.0, 8.0]]).T  # T is transpose matrix
+    print("Solving Ax = b with Gaussian elimination...")
+    print(f"A: {A}")
+    print(f"b: {b}")
+
+    x_np = np.linalg.solve(A, b)
+    print(f"The solution is: {x_np}")
+
+    gauss_e = gauss_solve(A, b)
+    print(f"The gauss_solve algorithm gives: {gauss_e}")
 
 
 if __name__ == "__main__":
